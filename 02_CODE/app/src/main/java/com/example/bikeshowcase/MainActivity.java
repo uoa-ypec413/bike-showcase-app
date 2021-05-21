ef8a5e91cce4d16991040bc20cc34e418d31f445
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2021-05-21 19:39:16 +1200

    Add ScrollView

--- 02_CODE/app/src/main/java/com/example/bikeshowcase/MainActivity.java
+++ 02_CODE/app/src/main/java/com/example/bikeshowcase/MainActivity.java
@@ -20,28 +20,28 @@ public class MainActivity extends AppCompatActivity {
         RelativeLayout roadCategory = findViewById(R.id.road_category);
         roadCategory.setOnClickListener(v -> {
             Intent roadCategoryIntent = new Intent(MainActivity.this, ListActivity.class);
-            roadCategoryIntent.putExtra(EXTRA_MESSAGE, "Road Bikes");
+            roadCategoryIntent.putExtra(EXTRA_MESSAGE, getString(R.string.road_category_name));
             startActivity(roadCategoryIntent);
         });
 
         RelativeLayout mountainCategory = findViewById(R.id.mountain_category);
         mountainCategory.setOnClickListener(v -> {
             Intent mountainCategoryIntent = new Intent(MainActivity.this, ListActivity.class);
-            mountainCategoryIntent.putExtra(EXTRA_MESSAGE, "Mountain Bikes");
+            mountainCategoryIntent.putExtra(EXTRA_MESSAGE, getString(R.string.mountain_category_name));
             startActivity(mountainCategoryIntent);
         });
 
         RelativeLayout adventureCategory = findViewById(R.id.adventure_category);
         adventureCategory.setOnClickListener(v -> {
             Intent adventureCategoryIntent = new Intent(MainActivity.this, ListActivity.class);
-            adventureCategoryIntent.putExtra(EXTRA_MESSAGE, "Adventure Bikes");
+            adventureCategoryIntent.putExtra(EXTRA_MESSAGE, getString(R.string.adventure_category_name));
             startActivity(adventureCategoryIntent);
         });
 
         RelativeLayout kidsCategory = findViewById(R.id.kids_category);
         kidsCategory.setOnClickListener(v -> {
             Intent kidsCategoryIntent = new Intent(MainActivity.this, ListActivity.class);
-            kidsCategoryIntent.putExtra(EXTRA_MESSAGE, "Kids Bikes");
+            kidsCategoryIntent.putExtra(EXTRA_MESSAGE, getString(R.string.kids_category_name));
             startActivity(kidsCategoryIntent);
         });
 
