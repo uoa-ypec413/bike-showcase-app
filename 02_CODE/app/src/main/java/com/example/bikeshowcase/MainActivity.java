7bcc596cf206b6736615321d97c8e46d165c40a8
Nick <68802819+uoa-nroe550@users.noreply.github.com>
2021-05-24 16:29:34 +1200

    Created the ItemAdapter class
    It makes cards for the listview containing the image, title, colour and price. Currently displayed in mainactivity just so I didn't have to make extra buttons from main into listactivity for testing

--- 02_CODE/app/src/main/java/com/example/bikeshowcase/MainActivity.java
+++ 02_CODE/app/src/main/java/com/example/bikeshowcase/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.bikeshowcase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Item> itemsList = DataProvider.getItems();
+        ItemAdapter itemsAdapter = new ItemAdapter(this,
+                R.layout.item_list_view_item, itemsList);
+        ListView listView = (ListView) findViewById(R.id.ListView);
+        listView.setAdapter(itemsAdapter);
     }
 }
