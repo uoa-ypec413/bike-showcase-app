bbc52a2d6b089c7fbd893e6845887c0e425ae8ab
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2021-05-29 18:11:45 +1200

    Add item fields to DetailsActivity

--- 02_CODE/app/src/main/java/com/example/bikeshowcase/DetailsActivity.java
+++ 02_CODE/app/src/main/java/com/example/bikeshowcase/DetailsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -16,14 +17,26 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        // Getting the first image for testing purposes, in actual implementation this will be provided
+        // Getting the first item for testing purposes, in actual implementation this will be provided
         // as an extra from the intent that starts this activity
         List<Item> items = DataProvider.getItems();
-        Item item = items.get(0);
+        Item item = items.get(7);
         String[] images = item.getImageFileNameList();
 
         ViewPager2 viewPager = findViewById(R.id.view_pager);
         mViewPagerAdapter = new ViewPagerAdapter(this, R.layout.image_view_pager, images);
         viewPager.setAdapter(mViewPagerAdapter);
+
+        TextView titleTextView = findViewById(R.id.details_title_view);
+        titleTextView.setText(item.getItemTitle());
+
+        TextView descriptionTextView = findViewById(R.id.details_colour_view);
+        descriptionTextView.setText(item.getColour().toString());
+
+        TextView priceTextView = findViewById(R.id.details_price_view);
+        priceTextView.setText("$" + item.getPrice().toString());
+
+        TextView colorTextView = findViewById(R.id.details_description_view);
+        colorTextView.setText(item.getDescription().toString());
     }
 }
