af0eeeddf2ea90cdebe67e0165d5d59eb918b4a5
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2021-06-02 19:35:02 +1200

    Add search functionality
    Exceptions currently not working

--- 02_CODE/app/src/main/java/com/example/bikeshowcase/ListActivity.java
+++ 02_CODE/app/src/main/java/com/example/bikeshowcase/ListActivity.java
@@ -5,26 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.List;
 
-public class ListActivity extends AppCompatActivity {
+public abstract class ListActivity extends AppCompatActivity {
+
+    protected List<Item> itemsList;
+    protected ItemAdapter itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
+        this.populateItemsList(message);
 
-        List<Item> itemsList = DataProvider.getCategoryItems(message);
-        ItemAdapter itemsAdapter = new ItemAdapter(this,
-                R.layout.item_list_view_item, itemsList);
-        ListView listView = (ListView) findViewById(R.id.ListView);
-        listView.setAdapter(itemsAdapter);
+        try{
+            this.itemsAdapter = new ItemAdapter(this, R.layout.item_list_view_item, this.itemsList);
+            ListView listView = findViewById(R.id.ListView);
+            listView.setAdapter(this.itemsAdapter);
+        } catch (NullPointerException e){
+            System.out.println(e.getMessage());
+        }
     }
+
+    public abstract void populateItemsList(String message);
 }
---
+++ 02_CODE/app/src/main/java/com/example/bikeshowcase/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.example.bikeshowcase;
+
+import android.os.Bundle;
+
+public class SearchActivity extends ListActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void populateItemsList(String message) {
+        this.itemsList = DataProvider.getSearchItems(message);
+    }
+}
