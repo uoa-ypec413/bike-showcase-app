2af3fef9060324fbd65e145de30a862d1bd185e2
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2021-06-02 22:28:53 +1200

    Add snackbar

--- 02_CODE/app/src/main/java/com/example/bikeshowcase/ListActivity.java
+++ 02_CODE/app/src/main/java/com/example/bikeshowcase/ListActivity.java
@@ -3,8 +3,12 @@ package com.example.bikeshowcase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -23,12 +27,21 @@ public abstract class ListActivity extends AppCompatActivity {
 
         this.populateItemsList(message);
 
-        try{
+        if(this.itemsList.isEmpty()){
+            View view = findViewById(R.id.list_constraint_layout);
+            Snackbar snackbar = Snackbar.make(view, "Sorry, no matches found!", Snackbar.LENGTH_LONG);
+            snackbar.setAction("GO BACK", v -> finish());
+            snackbar.setActionTextColor(Color.BLACK);
+            View snackBarView = snackbar.getView();
+            snackBarView.setBackgroundResource(R.drawable.background_primary_variant_round);
+            TextView textView = snackBarView.findViewById(com.google.android.material.R.id.snackbar_text);
+            textView.setTextColor(Color.BLACK);
+            snackbar.show();
+
+        } else {
             this.itemsAdapter = new ItemAdapter(this, R.layout.item_list_view_item, this.itemsList);
             ListView listView = findViewById(R.id.ListView);
             listView.setAdapter(this.itemsAdapter);
-        } catch (NullPointerException e){
-            System.out.println(e.getMessage());
         }
     }
 
