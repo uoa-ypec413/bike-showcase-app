f1c5f80dab6593e951a04785edc6e29ab335bb60
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2021-06-02 12:24:10 +1200

    Improve ListActivity aesthetics

--- 02_CODE/app/src/main/java/com/example/bikeshowcase/ListActivity.java
+++ 02_CODE/app/src/main/java/com/example/bikeshowcase/ListActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,15 +17,20 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
+        // Create a category header for the ListView
+        TextView textView = new TextView(this);
+        textView.setText(message);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextAppearance(R.style.TextAppearance_MaterialComponents_Headline5);
 
         List<Item> itemsList = DataProvider.getCategoryItems(message);
         ItemAdapter itemsAdapter = new ItemAdapter(this,
                 R.layout.item_list_view_item, itemsList);
-        ListView listView = (ListView) findViewById(R.id.ListView);
+        ListView listView = findViewById(R.id.list_view);
+        listView.addHeaderView(textView);
         listView.setAdapter(itemsAdapter);
     }
 }
